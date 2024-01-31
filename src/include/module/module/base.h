35782ac86ba2829018fe4c00715dacbcc027ee29
Dobrowolski, PawelX <pawelx.dobrowolski@intel.com>
2024-01-31 09:52:28 +0100

    necessary ifdefs added

--- src/include/module/module/base.h
+++ src/include/module/module/base.h
@@ -15,6 +15,8 @@
 
 #include "interface.h"
 #include "../ipc4/base-config.h"
+#include <list.h>
+#include <sof/audio/module_adapter/module/generic.h>
 
 #define module_get_private_data(mod) ((mod)->priv.private)
 #define module_set_private_data(mod, data) ((mod)->priv.private = data)
